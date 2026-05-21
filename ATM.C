6303431d8e47b49488b8e7e750b67a7ea9aae359
Ahmed <khateep15@gmail.com>
2026-05-21 15:18:47 +0300

    Russian ATM Bank

---
+++ ATM.C
@@ -0,0 +1,80 @@
+// Tinkoff Bank ATM (Russian Bank)
+// select an option 
+// 1check balance
+// 2deposit money 
+// 3withdraw money
+// 4exit 
+// enter your choice 
+#include<stdio.h>
+
+// Functions 
+void displaying (float balance);
+
+// =====================
+int main(){
+    float balance=0.0f;
+    printf("++++++++++++++++\n");
+    printf("Tinkoff Bank ATM\n");
+    printf("++++++++++++++++\n\n");
+// Calling
+    displaying(balance);
+    return 0;
+}
+
+void displaying (float balance){
+    int dep=0;
+    int withd=0;
+    int choice=0;
+    do{
+        printf("Select an option:\n");
+        printf("1.Check Balance\n");
+        printf("2.Deposit Money\n");
+        printf("3.Withdraw Money\n");
+        printf("4.Exit\n");
+        
+        printf("Enter Your Choice: ");
+        if(scanf("%d",&choice)!=1){
+            printf("\n!!!!!!!!!!!!!!!!!!\n");
+            printf("\nInvalid Choice !\n");
+            printf("Please enter only a number\n\n");
+            while(getchar()!='\n');
+            continue;
+        }
+     
+        switch(choice){
+            case 1:
+                printf("Your Current Balance is RUB %.2f \n\n",balance);
+                break;
+            case 2:
+                printf("Enter amount to deposit: ");
+                scanf("%d",&dep);
+                if(dep<0){
+                    printf("Invalid amount\n");
+                    continue;}
+                else if (dep>0){
+                    balance+=dep;
+                    printf("Deposited Successfully\n\n");
+                }
+                break;
+            case 3:
+                printf("Enter amount to withdraw: ");
+                scanf("%d",&withd);
+                if (withd<0){
+                    printf("Invalid amount\n");
+                    continue;}
+                else if (withd<=balance){
+                    balance-=withd;
+                    printf("You withdraw %d\n",withd);
+                    printf("Your Currenet Balance is RUB %.2f\n\n",balance);
+                }
+                else{
+                    printf("Insufficient Balance\n");
+                }
+                break;
+            case 4:
+                printf("\nExit Succssuflly \n");
+                break;
+        }
+    }while(choice!=4);
+}
+    
