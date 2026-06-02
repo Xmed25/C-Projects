415bdbdcc7ab23433f221688044519afd487e0f1
Ahmed <khateep15@gmail.com>
2026-06-02 15:46:34 +0300

    Refactor guessing game logic and structure

--- Guessing Game.c
+++ Guessing Game.c
@@ -2,37 +2,83 @@
 #include<stdlib.h>
 #include<time.h>
 
-int main(){
 
+int choice;
+int tries;
+int answer;
+
+void displaying();
+void processing();
+
+// ==============================
+int main() {
     srand(time(NULL));
-    int guess=0;
-    int tries=0;
-    int max=10;
-    int answer=rand() % max + 1 ;
+    answer=rand()%10 +1;
+    displaying(&choice);
+    processing(answer);
     
-    printf("***Welcome to Number Guessing Game*** \n\n");
-   do{
-    printf("Guess a number between (0-%d):",max);
-    if(scanf("%d",&guess)!=1){
-        printf("Please guess only a number\n");
-        while(getchar()!='\n');
-        continue;
-    }
-    
-    tries++;
-    
-    if(guess>answer){
-        printf("TOO HIGH !\n");
-    }
-    else if (guess<answer){
-        printf("TOO LOW !\n");
-    }
-
-   }while(guess!=answer);
-   
-    printf("Correct !");
-    printf("the answer is %d\n",answer);
-    printf("it took you %d tries",tries);
-   
-    return 0 ;
+    return 0;
 }
+
+void displaying(){
+    printf("=======================\n");
+    printf("Welcome to guess Game !\n");
+    printf("=======================\n");
+    
+    
+    
+    printf("1.Play\n");
+    printf("2.Exit\n");
+    printf("Enter Your Choice: ");
+    scanf("%d",&choice);
+}
+
+void processing(){
+    int guess;
+     switch(choice){
+            case 1:
+                do{
+                    printf("Guess a number between 1-10 : ");
+                    if(scanf("%d",&guess)!=1){
+                        printf("Error !\n");
+                        printf("Please Enter a number\n");
+                        while(getchar()!='\n');
+                        continue;
+                    }
+                    if(guess>10 || guess<1 ){
+                        printf("Please Enter a number between 1-10 !\n");
+                        continue;
+                    }
+                    
+                    tries++;
+                    if(guess==answer){
+                        printf("Correct answer!\n");
+                        printf("Computer Choice was %d\n",answer);
+                        printf("Number of tries: %d\n", tries);
+                        break;
+                    }
+                    
+                    else if (guess>answer){
+                        printf("Too High !\n");
+                        continue;
+                    }
+                    else if (guess<answer){
+                        printf("Too Low !\n");
+                        continue;
+                    }
+                    break;
+                    
+                }while(guess!=answer);
+                break;
+                
+            case 2:
+                printf("\nExit Successuflly !\n");
+                break;
+            
+            default:
+                printf("Invalid Choice !\n");
+        }   
+    
+}
+    
+
